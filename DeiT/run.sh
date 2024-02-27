e6b6fdf6700629a1f3ddb040bbbf3dd65c12dc2c
tamnguyen9520 <30925183+tamnguyen9520@users.noreply.github.com>
2024-02-26 20:42:51 -0600

    Update run.sh

--- DeiT/run.sh
+++ DeiT/run.sh
@@ -1,11 +1,11 @@
 
 ###### Reproducing our Imagenet Results
 ### BASELINE
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main_train.py\
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main.py \
 --model deit_tiny_patch16_224 --batch-size 256 --data-path path/to/data --output_dir some/random/path
 
 ### NEUTRENO
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main_train.py\
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main.py \
 --model deit_tiny_neutreno_patch16_224 --batch-size 256 --data-path path/to/data --output_dir some/random/path
 
 
